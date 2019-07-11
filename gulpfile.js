d44ecc6c415dc7ed6186f7cd5bdb1d7a9397bfd2
Svetlana <39729453+Lana-Light@users.noreply.github.com>
2019-07-11 18:49:40 +0300

    Add prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,134 @@
-var { watch, src, dest, series, parallel } = require('gulp');
-var browserSync = require('browser-sync').create();
-var del = require('del');
-var imagemin = require('gulp-imagemin');
-var advpng = require('imagemin-advpng');
-var cache = require('gulp-cache');
-var gulpif = require('gulp-if');
-var rename = require('gulp-rename');
+const { watch, src, dest, series, parallel } = require("gulp");
+const browserSync = require("browser-sync").create();
+const del = require("del");
+const imagemin = require("gulp-imagemin");
+const advpng = require("imagemin-advpng");
+const cache = require("gulp-cache");
+const gulpif = require("gulp-if");
+const rename = require("gulp-rename");
+const sourcemaps = require("gulp-sourcemaps");
 
-var pug = require('gulp-pug');
-var replace = require('gulp-replace');
-var sass = require('gulp-sass');
-var cssnano = require('gulp-cssnano');
-var babel = require('gulp-babel');
-var uglify = require('gulp-uglify-es').default;
-var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var b3 = browserify({ entries: ['./src/js/all.js'] });
+const pug = require("gulp-pug");
+const replace = require("gulp-replace");
+const sass = require("gulp-sass");
+const cssnano = require("gulp-cssnano");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify-es").default;
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const browserify = require("browserify")({ entries: ["./src/js/all.js"], debug: true });
+
+const isDev = process.argv.includes("dev") || process.argv.includes("js");
+console.log(isDev);
 
 function compJs() {
-    return src('src/js7/**')
-        .pipe(babel())
-        .pipe(dest('src/js'));
+  return src("src/js7/**/*.js")
+    .pipe(gulpif(isDev, sourcemaps.init()))
+    .pipe(babel())
+    .pipe(gulpif(isDev, sourcemaps.write()))
+    .pipe(dest("src/js"));
 }
 
 function bundle() {
-    return b3.bundle()
-        .pipe(source('all.js'))
-        .pipe(buffer())
-        .pipe(dest('src/js'));
+  return (
+    browserify
+      .bundle()
+      .pipe(source("all.js"))
+      .pipe(buffer())
+      .pipe(dest("src/js"))
+  );
 }
 
 function compSass() {
-    return src('src/scss/all.scss')
-        .pipe(sass())
-        .pipe(dest('src/css'));
+  return src("src/scss/all.scss")
+    .pipe(gulpif(isDev, sourcemaps.init()))
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(dest("src/css"));
 }
 
-var min = doFun => () => doFun()
-    .pipe(gulpif('*.css', cssnano(), uglify({ ecma: 6 })))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulpif('*.css', dest('dist/css'), dest('dist/js')));
+function min(doFun) {
+  return (
+    doFun()
+      .pipe(gulpif(isDev, browserSync.stream()))
+      .pipe(gulpif(isDev, sourcemaps.init({ loadMaps: true })))
+      .pipe(gulpif("*.css", cssnano()))
+      .pipe(gulpif("*.js", uglify({ ecma: 6 })))
+      .pipe(rename({ suffix: ".min" }))
+      .pipe(gulpif(isDev, sourcemaps.write()))
+      .pipe(gulpif("*.css", dest("dist/css")))
+      .pipe(gulpif("*.js", dest("dist/js")))
+  );
+}
+const minJs = () => min(bundle);
+const minCss = () => min(compSass);
 
-var brSync = async () => browserSync.init({ server: 'src' });
-var bsStream = doFun => () => doFun().pipe(browserSync.stream());
-
-var compPug = () => src('src/pug/index.pug')
+const compPug = () =>
+  src("src/pug/index.pug")
     .pipe(pug())
-    .pipe(dest('src'));
+    .pipe(dest("src"));
 
-var distView = () => compPug()
-    .pipe(replace('all.css', 'all.min.css'))
-    .pipe(replace('all.js', 'all.min.js'))
-    .pipe(dest('dist'));
+const distView = () =>
+  compPug()
+    .pipe(replace("all.css", "all.min.css"))
+    .pipe(replace("all.js", "all.min.js"))
+    .pipe(dest("dist"));
 
 async function watchWrap() {
-    watch('src/pug/**', compPug);
-    watch('src/scss/**', bsStream(min(compSass)));
-    watch('src/js7/**', series(compJs, bsStream(min(bundle))));
-    watch('src/*.html').on('change', browserSync.reload);
+  browserSync.init({ server: "src" });
+  watch("src/pug/**", distView);
+  watch("src/scss/**", minCss);
+  watch("src/js7/**", series(compJs, minJs));
+  watch("src/*.html").on("change", browserSync.reload);
 }
 
 function font() {
-    return src('src/fonts/**')
-        .pipe(dest('dist/fonts'));
+  return src("src/fonts/**").pipe(dest("dist/fonts"));
 }
 
 function image() {
-    return src('src/images/*.+(jpg|jpeg|png|gif|svg)')
-        .pipe(cache(imagemin([
+  return src("src/images/*.+(jpg|jpeg|png|gif|svg)")
+    .pipe(
+      cache(
+        imagemin(
+          [
             imagemin.gifsicle({ interlaced: true }),
             imagemin.jpegtran({ progressive: true }),
             advpng(),
             imagemin.svgo({
-                plugins: [
-                       { removeViewBox: true },
-                       { cleanupIDs: false }
-                ]
+              plugins: [{ removeViewBox: true }, { cleanupIDs: false }]
             })
-        ], { verbose: true }
-        )))
-        .pipe(dest('dist/images'));
+          ],
+          { verbose: true }
+        )
+      )
+    )
+    .pipe(dest("dist/images"));
 }
 
 async function clean() {
-    await del(['dist/**', '!dist', 'src/js/**', '!src/js', 'src/css/**', '!src/css']);
-    return cache.clearAll();
+  await del([
+    "dist/**",
+    "!dist",
+    "src/js/**",
+    "!src/js",
+    "src/css/**",
+    "!src/css"
+  ]);
+  return cache.clearAll();
 }
 
-var cleanDist = async () => del(['dist/**', '!dist', '!dist/images', '!dist/images/*']);
+var cleanDist = async () =>
+  del(["dist/**", "!dist", "!dist/images", "!dist/images/*"]);
 
+exports.build = series(
+  cleanDist,
+  parallel(font, image, distView, minCss, series(compJs, minJs))
+);
 
-exports.build = series(cleanDist, parallel(font, image, distView, min(compSass), series(compJs, min(bundle))));
-exports.dev = series(cleanDist, brSync, parallel(compPug, series(compJs, bsStream(bundle)), bsStream(compSass), watchWrap));
+exports.dev = series(
+  cleanDist,
+  parallel(distView, series(compJs, minJs), minCss, watchWrap)
+);
+
 exports.clean = clean;
